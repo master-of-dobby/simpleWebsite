11e57687a1a2634752dc45ba298ebe6536a92cb4
master-of-dobby <naveenoffcial777@gmail.com>
2024-01-14 06:44:22 +0530

    update using js dynamic website

--- index.js
+++ index.js
@@ -14,3 +14,63 @@ var menuItems = navLinks.getElementsByTagName('li');
         navLinks.classList.remove('active');
       });
     }
+
+const albums = document.getElementsByClassName('container')[0];
+const button = document.getElementById('addBtn');
+
+button.addEventListener('click', function(){
+  
+  const imageURL = document.getElementById('image').value;
+  const description = document.getElementById('desc').value;
+
+  const album = createAlbum(imageURL, description);
+  albums.appendChild(album);
+
+});
+
+function createAlbum(imageURL, description){
+
+  const albumDiv = document.createElement('div'); //<div>
+  albumDiv.className = 'album';
+
+  const image = document.createElement('img');
+  image.setAttribute('src', imageURL);
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = description;
+
+
+  const albumFooter = document.createElement('div');
+  albumFooter.className = 'albumFooter';
+
+  const buttonDiv = document.createElement('div');
+  const button1 = document.createElement('button');
+  const button2 = document.createElement('button');
+
+  button1.textContent = 'view';
+  button2.textContent = 'edit';
+
+  buttonDiv.appendChild(button1);
+  buttonDiv.appendChild(button2);
+
+  const timeElement = document.createElement('span');
+  timeElement.textContent = '9 min';
+
+  albumFooter.appendChild(buttonDiv);
+  albumFooter.appendChild(timeElement);
+
+
+  albumDiv.appendChild(image);
+  albumDiv.appendChild(paragraph);
+  albumDiv.appendChild(albumFooter);
+
+
+
+  return albumDiv;
+
+
+
+}
+
+//<div>
+//</div>
