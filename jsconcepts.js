651cc1b321f6db473ee00b710ef38eadfebc4138
master-of-dobby <naveenoffcial777@gmail.com>
2024-01-14 18:16:12 +0530

    changes resp

--- jsconcepts.js
+++ jsconcepts.js
@@ -1,8 +1,20 @@
-var a = 10;
+//var a = 10;
+//let a = 10;
+//a =12;
+const a = 20;
+
 console.log(a);
 
-function a() {
-  return a * a;
+function b() {
+  var x = 20;
+  return "ada" + a * a;
 }
 
-a();
+//console.log(x);
+
+function c() {
+  console.log(a * 20);
+}
+
+console.log(b());
+c();
