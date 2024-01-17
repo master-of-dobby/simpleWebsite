2ea05b559d1bec29a60e7c14c26f31b2192263e5
master-of-dobby <naveenoffcial777@gmail.com>
2024-01-17 17:39:55 +0530

    adding react

---
+++ async.js
@@ -0,0 +1,58 @@
+// console.log("Hello, World to NAANs");
+
+// setTimeout(function() {
+//     console.log("Thank you!");
+// }, 5000);
+
+// console.log("Enjoy the FOOD!");
+
+// const response = fetch('https://dummyjson.com/products/1');
+
+// response.then((result) => result.json());
+
+// response.catch(err => {
+//     console.log(err);
+// })
+
+// console.log(response);
+
+//  create promise
+
+// let success = true;
+// const orderId = "1234";
+
+// function testingPromise(orderId){
+
+//     const promise = new Promise(function (resolve, reject){
+
+//         if(success){
+//             console.log("promise is successfu!");
+//             resolve(orderId);
+//         }
+//         else{
+//             const error = new Error("Error in fetching data");
+//             reject(error);
+//         }
+//     })
+
+//     return promise;
+    
+// }
+
+
+// const result = testingPromise(orderId);
+// //const result1 = await anotherfUN();
+
+// result.then((result) => {
+//     console.log("OrderId", result);
+// }).catch(err => {
+//     console.log("Got an error: ", err);
+// })
+
+
+// console.log(result);
+
+
+
+
+
