e779be07206e1978a5cea8d1fe0f731e53ec30d0
master-of-dobby <naveenoffcial777@gmail.com>
2024-01-17 17:52:19 +0530

    adding react1

---
+++ hello.js
@@ -0,0 +1,14 @@
+
+const parent = React.createElement('div', {id : 'parent'},
+    React.createElement('div', {id : 'child'},  [
+        React.createElement('h1', {}, 'Hello, World! --- using React Elements'),
+        React.createElement('h1', {}, 'Okay , Bye! --- using React Elements')
+    ]
+        
+));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
+
+
+
